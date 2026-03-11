7955ca4672b950cae6c087668696dc83be5d2264
RamanSashyn <romasashin60@gmail.com>
2026-03-11 16:34:28 +0300

    Более красивый вывод в админ-панели

--- products/models.py
+++ products/models.py
@@ -18,4 +18,4 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
