afd687d6be896d3a19459ffaa07897a1d3293722
RamanSashyn <romasashin60@gmail.com>
2026-03-11 16:34:56 +0300

    Первая регистрация моделей с добавлением поиска по имени

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductCategory, Product
+
+@admin.register(ProductCategory)
+class ProductCategory(admin.ModelAdmin):
+    search_fields = ('name',)
+
+@admin.register(Product)
+class Category(admin.ModelAdmin):
+    search_fields = ('name',)
