39d3935bb51af8401385b3a6a7c3dad7b97525c9
RamanSashyn <romasashin60@gmail.com>
2026-03-11 17:06:54 +0300

    Правильное сохранение изображения в папку медиа через settings и urls

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits= 10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='media/products_images')
+    image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
