78cdbf0b46c25fe69d86947aef4a669d47507f88
RamanSashyn <romasashin60@gmail.com>
2026-03-11 18:53:51 +0300

    Улучшение админки(поиск, филтрация, отображение)

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import ProductCategory, Product
 
 @admin.register(ProductCategory)
-class ProductCategory(admin.ModelAdmin):
+class ProductCategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 @admin.register(Product)
-class Category(admin.ModelAdmin):
-    search_fields = ('name',)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'quantity', 'category')
+    search_fields = ('name', 'description',)
+    list_filter = ('category',)
