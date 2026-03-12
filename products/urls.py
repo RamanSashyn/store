4677658eee2661205f781af8bc66bfa29b2120ff
RamanSashyn <romasashin60@gmail.com>
2026-03-12 16:56:30 +0300

    Правильная работа с маршрутами(urls)

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
-    path('', views.products, name='products'),
+    path('', views.products, name='index'),
 ]
