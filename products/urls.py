2c78ee7a8c9c95b4a5be9cc8edf5be107e773183
RamanSashyn <romasashin60@gmail.com>
2026-03-17 17:03:46 +0300

    Подключение контроллера обработчика событий(добавление товара в корзину) к products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from products.views import products, basket_add
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.products, name='index'),
+    path('', products, name='index'),
+    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
 ]
