bdda01beb660c0bed320b70f70fe5b6b8f41c1cf
RamanSashyn <romasashin60@gmail.com>
2026-03-17 18:38:30 +0300

    Перененос логики для итоговой стоимости и количества товаров в правильное место(кастомный менеджер в модели)

--- users/views.py
+++ users/views.py
@@ -47,14 +47,10 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
 
-    baskets = Basket.objects.filter(user=request.user)
-
     context = {
         'title': 'Store - Профиль',
         'form': form,
-        'baskets': baskets,
-        'total_quantity': sum(basket.quantity for basket in baskets),
-        'total_sum': sum(basket.sum() for basket in baskets),
+        'baskets': Basket.objects.filter(user=request.user),
     }
     return render(request, 'users/profile.html', context)
 
