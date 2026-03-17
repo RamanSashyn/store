a9985d655ddabd357da22f90281fa2dfb9fc37b5
RamanSashyn <romasashin60@gmail.com>
2026-03-17 18:37:53 +0300

    Создание кастомного меннеджера для итоговой стоимости и количества товаров

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+from urllib import request
+
 from django.db import models
 
 from users.models import User
@@ -23,11 +25,21 @@ class Product(models.Model):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 
 
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
