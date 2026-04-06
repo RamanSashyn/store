2d79053945f3e4418a7543a2daf1f989b32d732a
RamanSashyn <romasashin60@gmail.com>
2026-04-06 18:55:00 +0300

    Контроллер CBV который отрабатывает при переходе по ссылке(проверяет есть ли такой списоки не просрочена ли ссылка) и если все корректно меняет верификацию в значение True

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
-from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
-from users.models import User
+from users.models import User, EmailVerification
 from common.views import TitleMixin
 
 
@@ -37,6 +39,24 @@ class UserProfileView(TitleMixin, UpdateView):
         context = super(UserProfileView, self).get_context_data()
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
+
+
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+
+    def get(self, request, *args, **kwargs): # при переходе по ссылке срабатывает get запрос
+        code = kwargs['code'] # достаем код
+        user = User.objects.get(email=kwargs['email']) # достаем пользователя по почте
+        email_verifications = EmailVerification.objects.filter(user=user, code=code)
+        # достаем через filter, а не get чтобы в случаи отсутсвия возвращался пустой список
+        if email_verifications.exists() and not email_verifications.first().is_expired():
+        # проверка существует ли нужный список и не просрочена ли ссылка
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('index'))
 
 
 # def logout(request):
