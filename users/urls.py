c03d6a53ff5375a9bb243203e0fa670968f35c2b
RamanSashyn <romasashin60@gmail.com>
2026-04-06 18:56:11 +0300

    Подключение контроллера(обязательные данные в ссылке <str:email>/<uuid:code>

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 
-from users.views import UserLoginView, UserRegistrationView, UserProfileView
+from users.views import UserLoginView, UserRegistrationView, UserProfileView, EmailVerificationView
 
 app_name = 'users'
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
+    # ссылка для проверки существования пользователя
 ]
