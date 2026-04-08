ca286552bddace1ff82da311795579546bdbd559
RamanSashyn <romasashin60@gmail.com>
2026-04-08 16:55:56 +0300

    Подключение БД пока что без .env

--- store/settings.py
+++ store/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_db',
+        'USER': 'store_username',
+        'PASSWORD': 'store_test@password2004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
