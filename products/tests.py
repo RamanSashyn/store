e07b91fc004ed823cc7007f58499cba81b089392
RamanSashyn <romasashin60@gmail.com>
2026-04-09 17:11:40 +0300

    Тест для IndexView

--- products/tests.py
+++ products/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+    # названия функции всегда должны начинаться на test
+    def test_view(self):
+        # в тестах нужно имитировать работу пользователя
+        path = reverse('index')
+        response = self.client.get(path) # делаем запрос на страницу
+
+        # метод assertEqual позволяет убедиться, что фактический результат работы функции или метода соответствует ожидаемому значению
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
