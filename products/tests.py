adc051390ce5a6ed40b5453ca3e1a13e421f0c69
RamanSashyn <romasashin60@gmail.com>
2026-04-09 17:13:30 +0300

    Тест для IndexView

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -10,6 +12,6 @@ class IndexViewTestCase(TestCase):
         response = self.client.get(path) # делаем запрос на страницу
 
         # метод assertEqual позволяет убедиться, что фактический результат работы функции или метода соответствует ожидаемому значению
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store')
         self.assertTemplateUsed(response, 'products/index.html')
