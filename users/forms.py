077ee0547c9124ecfd1de82c6c659be86ff523d4
RamanSashyn <romasashin60@gmail.com>
2026-04-10 17:15:16 +0300

    Сделал поля необязательными чтобы проходили тесты

--- users/forms.py
+++ users/forms.py
@@ -50,8 +50,8 @@ class UserProfileForm(UserChangeForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-label'}), required=False)
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
-    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}), required=False)
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': True}), required=False)
 
     class Meta:
         model = User
