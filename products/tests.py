1f6c1cd18e7e6e5d9fa67ebadbb5a835153894e3
RamanSashyn <romasashin60@gmail.com>
2026-04-10 17:14:56 +0300

    Дополнение тестов

--- products/tests.py
+++ products/tests.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
+from users.models import User
 
 
 class IndexViewTestCase(TestCase):
@@ -54,3 +55,31 @@ class ProductsListViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, 'products/products.html')
         self.assertEqual(response.context_data['title'], 'Store - Каталог')
+
+
+class BasketViewTestCase(TestCase):
+    fixtures = ['categories.json', 'products.json']
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='admin', password='123')
+
+    def test_add_to_basket(self):
+        product = Product.objects.first()
+        self.client.login(username='admin', password='123')
+        path = reverse('products:basket_add', args=[product.id])
+        response = self.client.post(path, HTTP_REFERER='/products/')
+
+        self.assertEqual(Basket.objects.count(), 1)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, '/products/')
+
+    def test_remove_from_basket(self):
+        product = Product.objects.first()
+        basket = Basket.objects.create(user=self.user, product=product)
+        self.client.login(username='admin', password='123')
+        path = reverse('products:basket_remove', args=[basket.id])
+        response = self.client.post(path, HTTP_REFERER=f'/users/profile/{self.user.id}/')
+
+        self.assertFalse(Basket.objects.filter(id=basket.id).exists())
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
--- users/tests.py
+++ users/tests.py
@@ -5,6 +5,7 @@ from urllib import response
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.timezone import now
+from allauth.socialaccount.models import SocialApp
 
 from users.models import User, EmailVerification
 
@@ -88,3 +89,65 @@ class UserRegistrationViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, 'Введённый пароль слишком широко распространён.', html=True)
         self.assertContains(response, 'Введённый пароль состоит только из цифр.', html=True)
+
+
+class UserLoginViewTestCase(TestCase):
+    def test_login(self):
+        User.objects.create_user(
+            username='roma',
+            password='12345678tT'
+        )
+
+        path = reverse('users:login')
+
+        response = self.client.post(path, {
+            'username': 'roma',
+            'password': '12345678tT',
+        })
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertTrue(response.wsgi_request.user.is_authenticated) # проверка что пользователь залогинился
+        self.assertRedirects(response, '/')
+
+
+class UserProfileViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name='Roma',
+            last_name='Sashyn',
+            username='roma',
+            password='12345678tT'
+        )
+        self.path = reverse('users:profile', kwargs={'pk': self.user.pk})
+
+    def test_user_profile_get(self):
+        self.client.login(username='roma', password='12345678tT')
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/profile.html')
+        self.assertEqual(response.context_data['title'], 'Store - Профиль')
+
+    def test_user_update_profile(self):
+        self.client.login(username='roma', password='12345678tT')
+        response = self.client.post(self.path, {'first_name': 'Oleh', 'last_name': 'Mizulo'})
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        expected_url = reverse('users:profile', kwargs={'pk': self.user.pk})
+        self.assertRedirects(response, expected_url)
+
+    def test_not_login_user_profile_get(self):
+        SocialApp.objects.create(
+            provider='github',
+            name='GitHub',
+            client_id='test',
+            secret='test'
+        )
+        response = self.client.get(self.path)
+
+        self.assertRedirects(response, f'/users/login/?next=/users/profile/{self.user.pk}/')
+
+
+
