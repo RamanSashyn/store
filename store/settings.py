e58a07cf1a1c447e8ede0de44a213b609c914ab7
RamanSashyn <romasashin60@gmail.com>
2026-04-14 18:42:52 +0300

    Использование и подключение отложенненной задачи для оптравки почты с помощью celery

--- store/settings.py
+++ store/settings.py
@@ -167,12 +167,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Sending emails
-# EMAIL_HOST = 'smtp.yandex.com'
-# EMAIL_PORT = 465
-# EMAIL_HOST_USER = 'Roomanhik98@yandex.by'
-# EMAIL_HOST_PASSWORD = 'gfexdbasafkddmhh'
-# EMAIL_USE_SSL = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'Roomanhik98@yandex.by'
+EMAIL_HOST_PASSWORD = 'gfexdbasafkddmhh'
+EMAIL_USE_SSL = True
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # OAuth
 
@@ -188,3 +188,8 @@ SOCIALACCOUNT_PROVIDERS = {
         ],
     }
 }
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
