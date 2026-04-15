40090fd03fa14d387c95274687085aab4701013f
RamanSashyn <romasashin60@gmail.com>
2026-04-15 17:33:18 +0300

    Контроллер для отображения страницы и сохранения данных

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from common.views import TitleMixin
+from orders.forms import OrderForm
+from orders.models import Order
 
 
-class OrderCreateView(TitleMixin, TemplateView):
+class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
     title = 'Store - Оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
 
 
 class OrderListView(TitleMixin, TemplateView):
