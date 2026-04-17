c6d2bcf518021b0b57aed6362310bae773bd89e7
RamanSashyn <romasashin60@gmail.com>
2026-04-17 19:07:03 +0300

    Измениние namespace

--- orders/urls.py
+++ orders/urls.py
@@ -7,7 +7,7 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
-    path('all/', OrderListView.as_view(), name='order_list'),
+    path('', OrderListView.as_view(), name='orders_list'),
     # path('<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('order_detail/', OrderDetailView.as_view(), name='order_detail'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
