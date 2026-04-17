f3e82d8cb57252dda8c887af804c73b6455dad2a
RamanSashyn <romasashin60@gmail.com>
2026-04-17 19:24:25 +0300

    Изменение контроллера для страницы деталей заказа

--- orders/urls.py
+++ orders/urls.py
@@ -8,8 +8,7 @@ app_name = 'orders'
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
     path('', OrderListView.as_view(), name='orders_list'),
-    # path('<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
-    path('order_detail/', OrderDetailView.as_view(), name='order_detail'),
+    path('order_detail/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
