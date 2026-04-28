8e291a48e323a40147ce47f75b539e15a60e6290
RamanSashyn <romasashin60@gmail.com>
2026-04-28 15:57:19 +0300

    Более правильная настройка для сбора статики

--- store/settings.py
+++ store/settings.py
@@ -187,10 +187,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
