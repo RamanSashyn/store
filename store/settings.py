5b0ba5b2a2d2be063559572b58ee52e9c03d8480
RamanSashyn <romasashin60@gmail.com>
2026-04-28 15:49:01 +0300

    Настройка для сбора статики на сервере при выключенном DEBUG

--- store/settings.py
+++ store/settings.py
@@ -186,9 +186,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
-if not DEBUG:
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
     STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
